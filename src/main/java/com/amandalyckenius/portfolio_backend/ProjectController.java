9815294172df76796057c82365eb2063b8672d5e
amandalyckenius <alyckenius@gmail.com>
2025-10-24 11:34:53 +0200

    feat(api): add ProjectController with dummy data

---
+++ src/main/java/com/amandalyckenius/portfolio_backend/ProjectController.java
@@ -0,0 +1,59 @@
+package com.amandalyckenius.portfolio_backend;
+
+import com.amandalyckenius.portfolio_backend.dto.ProjectResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/projects")
+public class ProjectController {
+
+    private final List<ProjectResponseDTO> projects = List.of(
+            new ProjectResponseDTO(
+                    "Vision Capsule",
+                    "Time-capsule web app that schedules messages for future delivery.",
+                    "vision-capsule",
+                    "https://github.com/AmandaLyckenius/vision-capsule",
+                    "https://visioncapsule.app",
+                    List.of("React", "TypeScript", "Spring Boot", "MongoDB")
+
+            ),
+            new ProjectResponseDTO(
+                    "Lagom Kul",
+                    "'lagom' fun app with randomised jokes",
+                    "lagom-kul",
+                    "https://github.com/BugBusters-G1/agprojekt",
+                    "https://agprojekt.vercel.app",
+                    List.of("React", "TypeScript", "Spring Boot", "MongoDB")
+            ),
+            new ProjectResponseDTO(
+                    "ToDo app",
+                    "ToDo app that helps you organize your tasks",
+                    "ws-app",
+                    "https://github.com/AmandaLyckenius/ws_app",
+                    "",
+                    List.of("React", "TypeScript", "Spring Boot", "MongoDB")
+
+            ));
+
+
+    @GetMapping
+    public List<ProjectResponseDTO> getAllProjects() {
+        return projects;
+    }
+
+    @GetMapping("/{slug}")
+    public ProjectResponseDTO getProjectBySlug(@PathVariable String slug) {
+        return projects.stream()
+                .filter(p -> p.slug().equals(slug))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Project not found: " + slug
+                ));
+    }
+
+}
