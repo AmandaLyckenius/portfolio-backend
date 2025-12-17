a94dc7c22a5d2259b1e1e928e7b43f40842f92d3
amandalyckenius <alyckenius@gmail.com>
2025-12-17 19:00:06 +0100

    feat(contactService): add logging

--- src/main/java/com/amandalyckenius/portfolio_backend/service/ContactService.java
+++ src/main/java/com/amandalyckenius/portfolio_backend/service/ContactService.java
@@ -1,6 +1,8 @@
 package com.amandalyckenius.portfolio_backend.service;
 
 import com.amandalyckenius.portfolio_backend.dto.ContactRequestDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,6 +14,7 @@ public class ContactService {
     private final JavaMailSender javaMailSender;
     private final String toAddress;
     private final String fromAddress;
+    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
 
     public ContactService(JavaMailSender javaMailSender, @Value("${portfolio.contact.to}") String toAddress, @Value("${portfolio.contact.from}") String fromAddress) {
         this.javaMailSender = javaMailSender;
@@ -30,6 +33,10 @@ public class ContactService {
         message.setReplyTo(email);
         message.setSubject("New portfolio contact from " + name);
 
+        String maskedEmail = maskEmail(email);
+
+        log.info("Received contact request from {} <{}>", name, maskedEmail);
+
         message.setText("""
             Name: %s
             Email: %s
@@ -40,7 +47,12 @@ public class ContactService {
 
         javaMailSender.send(message);
 
+        log.info("Successfully sent contact email for {}", maskedEmail);
 
+    }
+
+    private String maskEmail(String email) {
+        return email.replaceAll("(?<=.).(?=[^@]*?@)", "*");
     }
 
 }
